f4e34d292ab601b6e263e2ce1e3e6e9cfb7d0fb5
kumm <kumm@webstar.hu>
2015-10-17 11:56:34 +0200

    aIBlind

--- src/main/java/org/leanpoker/player/State.java
+++ src/main/java/org/leanpoker/player/State.java
@@ -14,9 +14,10 @@ public class State {
     private List<Card> holeCards;
     private Integer stack;
     private Integer minimumRaise;
+    private Integer myNdx;
 
     public void update(GameState gameState) {
-        Integer myNdx = gameState.getInAction();
+        myNdx = gameState.getInAction();
         com.wcs.poker.gamestate.Player me = gameState.getPlayers().get(myNdx);
         bet = me.getBet();
         holeCards = me.getHoleCards();
@@ -38,5 +39,17 @@ public class State {
 
     public Integer getMinimumRaise() {
         return minimumRaise;
+    }
+
+    public boolean amIBlind() {
+        return amIBigBlind() || amISmallBlind();
+    }
+
+    public boolean amISmallBlind() {
+        return myNdx == 0;
+    }
+
+    public boolean amIBigBlind() {
+        return myNdx == 1;
     }
 }
