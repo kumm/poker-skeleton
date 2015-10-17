c58e19fb8803146be43e86211a7a4d99af2105bc
kumm <kumm@webstar.hu>
2015-10-17 10:43:02 +0200

    1:kamikaze

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -4,11 +4,15 @@ import com.wcs.poker.gamestate.GameState;
 
 public class Player {
 
-    static final String VERSION = "Default Java folding player";
+    static final String VERSION = "1:kamikaze";
 
     public static int betRequest(GameState gameState) {
-        return 0;
+//        Integer myNdx = gameState.getInAction();
+//        com.wcs.poker.gamestate.Player me = gameState.getPlayers().get(myNdx);
+        
+        return gameState.getMinimumRaise();
     }
+
 
     public static void showdown(GameState gameState) {
     }
--- src/test/java/org/leanpoker/player/PlayerTest.java
+++ src/test/java/org/leanpoker/player/PlayerTest.java
@@ -1,15 +1,17 @@
 package org.leanpoker.player;
 
 import com.wcs.poker.gamestate.GameState;
-import static org.junit.Assert.assertEquals;
 import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
 
 public class PlayerTest {
 
     @Test
     public void testBetRequest() throws Exception {
         final GameState gameState = new GameState();
+        gameState.setMinimumRaise(10);
 
-        assertEquals(0, Player.betRequest(gameState));
+        assertEquals(gameState.getMinimumRaise().intValue(), Player.betRequest(gameState));
     }
 }
